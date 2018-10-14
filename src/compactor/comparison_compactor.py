806960eabe26a3e34fb0f8b17629df07710461f7
jjhenson <abchenson@gmail.com>
2018-10-14 17:04:45 +0100

    complete unclean example close to the book's Java version

--- src/compactor/comparison_compactor.py
+++ src/compactor/comparison_compactor.py
@@ -1,10 +1,14 @@
 from typing import Optional
 
+
 class AssertDummy:
     @staticmethod
-    def format(msg: Optional[str], expect: str, actual:str) -> str:
-        msg = '' if msg is None else msg + " "
+    def format(msg: Optional[str], expect: Optional[str], actual: Optional[str]) -> str:
+        msg = '' if msg is None else msg + ' '
+        expect = 'None' if expect is None else expect
+        actual = 'None' if actual is None else actual
         return msg + "expected:<" + expect + "> but was:<" + actual + ">"
+
 
 class ComparisonCompactor:
     ELLIPSIS = "..."
@@ -18,7 +22,7 @@ class ComparisonCompactor:
         self.f_prefix: int = 0
         self.f_suffix: int = 0
 
-    def compact(self, message: str) -> str:
+    def compact(self, message: Optional[str]=None) -> str:
         if self.f_expected is None or self.f_actual is None or self._are_strings_equal():
             return AssertDummy.format(message, self.f_expected, self.f_actual)
         self._find_common_prefix()
@@ -28,29 +32,31 @@ class ComparisonCompactor:
         return AssertDummy.format(message, expected, actual)
 
     def _compact_string(self, source: str) -> str:
-        result = self.DELTA_START + source[self.f_prefix : len(source) - self.f_suffix + 1] + self.DELTA_END
+        result = self.DELTA_START + source[self.f_prefix: len(source) - self.f_suffix + 1] + self.DELTA_END
         if self.f_prefix > 0:
             result = self._compute_common_prefix() + result
-        if self.f_suffix > 0:
+        if self.f_suffix - 1 > 0:
             result = result + self._compute_common_suffix()
         return result
 
     def _find_common_prefix(self):
+        min_len = min(len(self.f_expected), len(self.f_actual))
         try:
-            self.f_prefix = next(i for i, (a1, a2) in enumerate(zip(self.f_expected, self.f_actual)) if a1 != a2)
+            self.f_prefix = next(i for i in range(min_len) if self.f_expected[i] != self.f_actual[i])
         except StopIteration:
             self.f_prefix = min(len(self.f_expected), len(self.f_actual))
 
     def _find_common_suffix(self):
         min_len = min(len(self.f_expected), len(self.f_actual))
         try:
-            self.f_suffix = next(i for i in range(1, min_len + 1) if self.f_expected[-i] != self.f_actual[-i])
+            self.f_suffix = next(i for i in range(1, min_len + 1) if self.f_expected[-i] != self.f_actual[-i] or
+                                 min_len - i + 1 == self.f_prefix)
         except StopIteration:
-            self.f_suffix = min(len(self.f_expected), len(self.f_actual))
+            self.f_suffix = min(len(self.f_expected), len(self.f_actual)) + 1
 
     def _compute_common_prefix(self) -> str:
         return (self.ELLIPSIS if self.f_prefix > self.f_context_length else "") + \
-            self.f_expected[max(0,self.f_prefix - self.f_context_length): self.f_prefix]
+            self.f_expected[max(0, self.f_prefix - self.f_context_length): self.f_prefix]
 
     def _compute_common_suffix(self):
         end = len(self.f_expected) - max(self.f_suffix - 1 - self.f_context_length, 0)
--- tests/test_comparison_compactor.py
+++ tests/test_comparison_compactor.py
@@ -1,13 +1,96 @@
 from compactor.comparison_compactor import ComparisonCompactor
 
+
 def test_message():
     failure = ComparisonCompactor(0, "b", "c").compact("a")
     assert(failure == "a expected:<[b]> but was:<[c]>")
 
+
 def test_start_same():
-    failure = ComparisonCompactor(1, "ba", "bc").compact(None)
-    assert (failure == "expected:<b[a]> but was:<b[c]>")
+    failure = ComparisonCompactor(1, "ba", "bc").compact()
+    assert(failure == "expected:<b[a]> but was:<b[c]>")
+
 
 def test_end_same():
-    failure = ComparisonCompactor(1, "ab", "cb").compact(None)
-    assert (failure == "expected:<[a]b> but was:<[c]b>")
+    failure = ComparisonCompactor(1, "ab", "cb").compact()
+    assert(failure == "expected:<[a]b> but was:<[c]b>")
+
+
+def test_same():
+    failure = ComparisonCompactor(1, "ab", "ab").compact()
+    assert(failure == "expected:<ab> but was:<ab>")
+
+
+def test_no_context_start_and_end_same():
+    failure = ComparisonCompactor(0, "abc", "adc").compact()
+    assert (failure == "expected:<...[b]...> but was:<...[d]...>")
+
+
+def test_start_and_end_context():
+    failure = ComparisonCompactor(1, "abc", "adc").compact()
+    assert (failure == "expected:<a[b]c> but was:<a[d]c>")
+
+
+def test_start_and_end_context_with_ellipses():
+    failure = ComparisonCompactor(1, "abcde", "abfde").compact()
+    assert (failure == "expected:<...b[c]d...> but was:<...b[f]d...>")
+
+
+def test_comparison_error_start_same_complete():
+    failure = ComparisonCompactor(2, "ab", "abc").compact()
+    assert (failure == "expected:<ab[]> but was:<ab[c]>")
+
+
+def test_comparison_error_end_same_complete():
+    failure = ComparisonCompactor(0, "bc", "abc").compact()
+    assert (failure == "expected:<[]...> but was:<[a]...>")
+
+
+def test_comparison_error_end_same_complete_context():
+    failure = ComparisonCompactor(2, "bc", "abc").compact()
+    assert (failure == "expected:<[]bc> but was:<[a]bc>")
+
+
+def test_comparison_error_overlapping_matches():
+    failure = ComparisonCompactor(0, "abc", "abbc").compact()
+    assert (failure == "expected:<...[]...> but was:<...[b]...>")
+
+
+def test_comparison_error_overlapping_matches_context():
+    failure = ComparisonCompactor(2, "abc", "abbc").compact()
+    assert (failure == "expected:<ab[]c> but was:<ab[b]c>")
+
+
+def test_comparison_error_overlapping_matches2():
+    failure = ComparisonCompactor(0, "abcdde", "abcde").compact()
+    assert (failure == "expected:<...[d]...> but was:<...[]...>")
+
+
+def test_comparison_error_overlapping_matches2_context():
+    failure = ComparisonCompactor(2, "abcdde", "abcde").compact()
+    assert (failure == "expected:<...cd[d]e> but was:<...cd[]e>")
+
+
+def test_comparison_error_with_actual_none():
+    failure = ComparisonCompactor(0, "a", None).compact()
+    assert (failure == "expected:<a> but was:<None>")
+
+
+def test_comparison_error_with_actual_none_context():
+    failure = ComparisonCompactor(2, "a", None).compact()
+    assert (failure == "expected:<a> but was:<None>")
+
+
+def test_comparison_error_with_expected_none():
+    failure = ComparisonCompactor(0, None, "a").compact()
+    assert (failure == "expected:<None> but was:<a>")
+
+
+def test_comparison_error_with_expected_none_context():
+    failure = ComparisonCompactor(2, None, "a").compact()
+    assert(failure == "expected:<None> but was:<a>")
+
+
+def test_bug_609972():
+    failure = ComparisonCompactor(10, "S&P500", "0").compact()
+    assert (failure == "expected:<[S&P50]0> but was:<[]0>")
